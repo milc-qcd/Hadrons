f4ded486b9d9e14d819c4d7030eee07c4339e3f6
Michael628 <michaellynch628@gmail.com>
2025-02-04 12:11:20 -0800

    Restore MesonF.cpp

--- Hadrons/Modules/MContraction/MesonF.cpp
+++ Hadrons/Modules/MContraction/MesonF.cpp
@@ -1,7 +1,7 @@
 /*
  * Meson.cpp, part of Hadrons (https://github.com/aportelli/Hadrons)
  *
- * Copyright (C) 2015 - 2023
+ * Copyright (C) 2015 - 2020
  *
  * Author: Antonin Portelli <antonin.portelli@me.com>
  *
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Hadrons.  If not, see <http://www.gnu.org/licenses/>.
  *
- * See the full license in the file "LICENSE" in the top level distribution 
+ * See the full license in the file "LICENSE" in the top level distribution
  * directory.
  */
 
